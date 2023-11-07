215975f2863829beabd08cccf289b869eaff56e1
kenny <kenny.kwyiu@gmail.com>
2023-11-07 14:32:33 +0800

    7 Nov 2023 implemented Queue by array

---
+++ src/main/java/org/kenny/data_strucute/queue/Main.java
@@ -0,0 +1,29 @@
+package org.kenny.data_strucute.queue;
+
+public class Main {
+    // ArrayQueue (ArrayDeque)
+    // enqueue
+    // dequeue
+    // peek
+    // isEmpty
+    // isFull
+    // [10, 20, 30, 40, 50]
+    //   F              R
+    // F = 0
+    // R = 4
+    public static void main(String[] args) {
+        ArrayQueue queue = new ArrayQueue(5);
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        queue.dequeue();
+        int front = queue.dequeue();
+        queue.enqueue(40);
+        queue.enqueue(50);
+        queue.enqueue(60);
+        queue.enqueue(70);
+        System.out.println(front);
+        System.out.println(queue);
+
+    }
+}
