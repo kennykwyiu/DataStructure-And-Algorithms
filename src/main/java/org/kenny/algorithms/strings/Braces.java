06f179fedfb5615027469c0605d05986b040285b
kenny <kenny.kwyiu@gmail.com>
2023-11-24 11:51:50 +0800

    24 Nov 2023 Braces - To complete the code provided and make it a valid solution for the "Braces" problem - check if the string s contains balanced braces.

---
+++ src/main/java/org/kenny/algorithms/strings/Braces.java
@@ -0,0 +1,36 @@
+package org.kenny.algorithms.strings;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ To complete the code provided and make it a valid solution for the "Braces" problem,
+ check if the string s contains balanced braces.
+ */
+
+public class Braces {
+    static String isBalanced(String s) {
+        if (s.length() % 2 != 0) {
+            return "NO";
+        }
+
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> bracesMap = new HashMap<>();
+        bracesMap.put('}', '{');
+        bracesMap.put(']', '[');
+        bracesMap.put(')', '(');
+
+        for (char c : s.toCharArray()) {
+            if (bracesMap.containsValue(c)) {
+                stack.push(c);
+            } else if (bracesMap.containsKey(c)) {
+                if (stack.isEmpty() || stack.pop() != bracesMap.get(c)) {
+                    return "NO";
+                }
+            }
+        }
+
+        return stack.isEmpty() ? "YES" : "NO";
+    }
+}
