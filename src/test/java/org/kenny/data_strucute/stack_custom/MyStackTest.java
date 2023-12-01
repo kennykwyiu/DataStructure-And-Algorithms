9e0934ffd011c2235a460c088999f284e7177754
kenny <kenny.kwyiu@gmail.com>
2023-12-01 08:10:46 +0800

    1 Dec 2023 MyStackTest

--- src/test/java/org/kenny/data_strucute/stack_custom/MyStackTest.java
+++ src/test/java/org/kenny/data_strucute/stack_custom/MyStackTest.java
@@ -4,13 +4,19 @@ import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 
-import java.util.EmptyStackException;
+import java.util.NoSuchElementException;
 
 public class MyStackTest {
-    private MyStack<Integer> stack = new MyStack<>();
+//    private StackInterface<Integer> stack;
+
+//    @BeforeEach
+//    public void setUp() {
+//        stack = new MyStack<>();
+//    }
 
     @Test
     public void testPushAndPop() {
+        MyStack<Integer> stack = new MyStack<>();
         stack.push(1);
         stack.push(2);
         stack.push(3);
@@ -22,6 +28,8 @@ public class MyStackTest {
 
     @Test
     public void testPushAndPeak() {
+        MyStack<Integer> stack = new MyStack<>();
+
         stack.push(10);
         stack.push(20);
 
@@ -35,11 +43,44 @@ public class MyStackTest {
 
     @Test
     public void testPopEmptyStack() {
-        Assertions.assertThrows(EmptyStackException.class, () -> stack.pop());
+        MyStack<Integer> stack = new MyStack<>();
+
+        Assertions.assertThrows(NoSuchElementException.class, () -> stack.pop());
     }
 
     @Test
     public void testPeakEmptyStack() {
-        Assertions.assertThrows(EmptyStackException.class, () -> stack.peak());
+        MyStack<Integer> stack = new MyStack<>();
+
+        Assertions.assertThrows(NoSuchElementException.class, () -> stack.peak());
+    }
+
+    @Test
+    public void testPushAndClear() {
+        MyStack<Integer> stack = new MyStack<>();
+
+        stack.push(5);
+        stack.push(10);
+        stack.push(15);
+
+        stack.clear();
+
+        Assertions.assertThrows(NoSuchElementException.class, () -> stack.pop());
+        Assertions.assertThrows(NoSuchElementException.class, () -> stack.peak());
+    }
+
+    @Test
+    public void testIsEmpty() {
+        MyStack<Integer> stack = new MyStack<>();
+
+        Assertions.assertTrue(stack.isEmpty());
+
+        stack.push(1);
+
+        Assertions.assertFalse(stack.isEmpty());
+
+        stack.pop();
+
+        Assertions.assertTrue(stack.isEmpty());
     }
 }
