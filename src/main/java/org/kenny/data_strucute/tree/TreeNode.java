0f3457261c0d48bc5f215e8e7801399438f10614
kenny <kenny.kwyiu@gmail.com>
2023-12-16 23:26:39 +0800

    16 Dec 2023 TreeNode

---
+++ src/main/java/org/kenny/data_strucute/tree/TreeNode.java
@@ -0,0 +1,11 @@
+package org.kenny.data_strucute.tree;
+
+public class TreeNode {
+    public int value;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int value) {
+        this.value = value;
+    }
+}
