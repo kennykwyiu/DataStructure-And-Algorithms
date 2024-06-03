b284e16dc194b2f8bf013d4a9e6979036b6a4f54
kennykwyiu <kenny.kwyiu@gmail.com>
2024-06-03 21:59:13 +0800

    3 Jun2 2024 BalancedBrackets

---
+++ src/main/java/org/kenny/algorithms/BalancedBrackets.java
@@ -0,0 +1,61 @@
+package org.kenny.algorithms;
+
+
+import java.io.*;
+import java.util.Stack;
+import java.util.stream.IntStream;
+
+public class BalancedBrackets {
+
+    /*
+     * Complete the 'isBalanced' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String isBalanced(String s) {
+        // Write your code here
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return "NO";
+                }
+                char top = stack.pop();
+                if ((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[')) {
+                    return "NO";
+                }
+
+            }
+        }
+            return stack.isEmpty() ? "YES" : "NO";
+    }
+
+    class Solution {
+        public static void main(String[] args) throws IOException {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+            int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+            IntStream.range(0, t).forEach(tItr -> {
+                try {
+                    String s = bufferedReader.readLine();
+
+                    String result = BalancedBrackets.isBalanced(s);
+
+                    bufferedWriter.write(result);
+                    bufferedWriter.newLine();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            });
+
+            bufferedReader.close();
+            bufferedWriter.close();
+        }
+    }
+
