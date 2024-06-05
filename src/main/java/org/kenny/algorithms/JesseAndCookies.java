c5d306c0adf375ff64bc0f94059a46b60e1278cb
kennykwyiu <kenny.kwyiu@gmail.com>
2024-06-05 21:56:09 +0800

    5 Jun 2024 JesseAndCookies

---
+++ src/main/java/org/kenny/algorithms/JesseAndCookies.java
@@ -0,0 +1,41 @@
+package org.kenny.algorithms;
+
+/**
+ *
+ * Problem:
+ * Jesse loves cookies. He wants the sweetness of all his cookies to be greater than value `K`. To do this, Jesse repeatedly mixes two of the cookies in his hand. Each cookie has a sweetness value which is a positive integer. The sweetness of two cookies is the sum of their individual sweetness values.
+ *
+ * You are given an array of sweetness values of cookies and the value `K`. Your task is to determine the minimum number of operations required to achieve Jesse's goal. If it is not possible to do so, return -1.
+ *
+ * Write a function `cookies` to find the minimum number of operations required to make all the cookies have sweetness greater than or equal to `K`.
+ *
+ * Function Signature:
+ * `int cookies(int k, int[] A)`
+ *
+ * Input:
+ * - An integer `k` representing the desired minimum sweetness value.
+ * - An array `A` of integers representing the sweetness values of the cookies. The length of the array is between 1 and 10^6.
+ *
+ * Output:
+ * - Return an integer representing the minimum number of operations required to achieve Jesse's goal. If it is not possible, return -1.
+ *
+ * Example:
+ * Input:
+ * `k = 7`
+ * `A = [1, 2, 3, 9, 10, 12]`
+ * Output:
+ * `2`
+ *
+ * Explanation:
+ * - Initially, the sweetness values are [1, 2, 3, 9, 10, 12].
+ * - After two operations, the sweetness values become [13, 12, 9], which are all greater than or equal to 7.
+ *
+ * Note:
+ * - The array `A` can contain duplicates.
+ * - The operations can be performed on any pair of cookies at a time.
+ * - The sweetness values of the cookies may change after each operation.
+ * - If it is not possible to achieve Jesse's goal, return -1.
+ *
+ */
+public class JesseAndCookies {
+}
