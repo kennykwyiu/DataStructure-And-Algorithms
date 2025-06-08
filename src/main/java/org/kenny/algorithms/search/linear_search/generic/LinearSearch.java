3d353ff6c4063dab80f2fdd08812d6fff4431859
kennyyiu <kenny.kwyiu@gmail.com>
2025-06-08 20:17:52 +0800

    add test

--- src/main/java/org/kenny/algorithms/search/linear_search/generic/LinearSearch.java
+++ src/main/java/org/kenny/algorithms/search/linear_search/generic/LinearSearch.java
@@ -1,5 +1,7 @@
 package org.kenny.algorithms.search.linear_search.generic;
 
+import org.kenny.algorithms.search.linear_search.ArrayGenerator;
+
 public class LinearSearch {
 
     public static <E> int search (E[] array, E target) {
@@ -11,7 +13,18 @@ public class LinearSearch {
         return -1;
     }
     public static void main(String[] args) {
-        Integer[] data = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        System.out.println(LinearSearch.search(data, 10));
+        int[] dataSize = {1000000, 10000000};
+        for (int n : dataSize) {
+            Integer[] data = ArrayGenerator.generateOrderedArray(n);
+
+            long startTime = System.nanoTime();
+            for (int i = 0; i < 100; i++) {
+                LinearSearch.search(data, n);
+            }
+            long endTime = System.nanoTime();
+
+            double time = (endTime - startTime) / 1000000000.0;
+            System.out.println("n = " + n + ", 100 runs: " + time + " seconds");
+        }
     }
 }
