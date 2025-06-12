5514cdf1e06ed9fe31baf270997a6a04771345e6
kennyyiu <kenny.kwyiu@gmail.com>
2025-06-12 21:28:07 +0800

    sortTest

--- src/main/java/org/kenny/algorithms/sorting/selectionSort/generic/SortingHelper.java
+++ src/main/java/org/kenny/algorithms/sorting/selectionSort/generic/SortingHelper.java
@@ -1,15 +1,38 @@
 package org.kenny.algorithms.sorting.selectionSort.generic;
 
+import org.kenny.algorithms.sorting.insertionSort.InsertionSort;
+
 public class SortingHelper {
     private SortingHelper() {
     }
 
-    public static <T extends Comparable<T>> boolean isSort(T[] array) {
+    public static <T extends Comparable<T>> boolean isSorted(T[] array) {
         for (int i = 1; i < array.length; i++) {
             if (array[i - 1].compareTo(array[i]) > 0) {
                 return false;
             }
         }
         return true;
+    }
+
+    public static <T extends Comparable<T>> void sortTest(String sortName, T[] array) {
+        long startTime = System.nanoTime();
+        if ("SelectionSort".equals(sortName)) {
+            SelectionSort.sort(array);
+        } else if ("InsertionSort".equals(sortName)) {
+            InsertionSort.sort(array);
+        } else if ("OptimizedInsertionSort".equals(sortName)) {
+            InsertionSort.optimizedSort(array);
+        }
+        long endTime = System.nanoTime();
+        double time = (endTime - startTime) / 1000000000.0;
+
+        if (!SortingHelper.isSorted(array)) {
+            throw new RuntimeException(sortName + " is failed");
+        }
+        System.out.printf("%s, n = %d : %f%n",
+                sortName,
+                array.length,
+                time);
     }
 }
