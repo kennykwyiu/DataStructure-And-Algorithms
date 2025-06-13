aa1a6c5ce8560981a2339bf416ef8a0b4b3e8085
kennyyiu <kenny.kwyiu@gmail.com>
2025-06-13 21:50:02 +0800

    InsertionSort

---
+++ src/main/java/org/kenny/algorithms/sorting/insertionSort/InsertionSort.java
@@ -0,0 +1,44 @@
+package org.kenny.algorithms.sorting.insertionSort;
+
+import org.kenny.algorithms.sorting.selectionSort.generic.ArrayGenerator;
+import org.kenny.algorithms.sorting.selectionSort.generic.SortingHelper;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    private InsertionSort() {
+    }
+
+    public static <T extends Comparable<T>> void sort(T[] array) {
+        for (int i = 0; i < array.length; i++) {
+//            for (int j = i; j - 1 >= 0; j--) {
+//                if (array[j].compareTo(array[j - 1]) < 0) {
+//                    swap(array, j, j - 1);
+//                } else {
+//                    break;
+//                }
+//            }
+
+            for (int j = i; j - 1 >= 0 && array[j].compareTo(array[j - 1]) < 0; j--) {
+                swap(array, j, j - 1);
+            }
+        }
+    }
+
+
+    private static <E> void swap(E[] arr, int i, int j) {
+        if (i != j) {
+            E temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] dataSize = {10000, 100000};
+        for (int i : dataSize) {
+            Integer[] randomArray = ArrayGenerator.generateRandomArray(i, i);
+            SortingHelper.sortTest("InsertionSort", randomArray);
+        }
+    }
+}
