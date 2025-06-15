420d3b04b5a643d77c75ca2336d393b9422f6ce1
kennyyiu <kenny.kwyiu@gmail.com>
2025-06-15 15:00:45 +0800

    test custom object

--- src/main/java/org/kenny/data_structure/course/datastrutures/array/generic/Main.java
+++ src/main/java/org/kenny/data_structure/course/datastrutures/array/generic/Main.java
@@ -1,26 +1,39 @@
 package org.kenny.data_structure.course.datastrutures.array.generic;
 
 
+import org.kenny.algorithms.sorting.selectionSort.comparable.Student;
+
 public class Main {
     public static void main(String[] args) {
-        Array<Integer> array = new Array<>(20);
-        for (int i = 0; i < 10; i++) {
-            array.addLast(i);
-        }
-        System.out.println(array);
-        array.add(1, 100);
-        System.out.println(array);
+//        Array<Integer> array = new Array<>(20);
+//        for (int i = 0; i < 10; i++) {
+//            array.addLast(i);
+//        }
+//        System.out.println(array);
+//        array.add(1, 100);
+//        System.out.println(array);
+//
+//        array.addFirst(-1);
+//        System.out.println(array);
+//
+//        array.remove(2);
+//        System.out.println(array);
+//
+//        array.removeElement(4);
+//        System.out.println(array);
+//
+//        array.removeFirst();
+//        System.out.println(array);
 
-        array.addFirst(-1);
-        System.out.println(array);
+        Array<Student> students = new Array<>();
+        students.addLast(new Student("Alice", 100));
+        students.addLast(new Student("Bob", 66));
+        students.addLast(new Student("Carl", 88));
 
-        array.remove(2);
-        System.out.println(array);
+        System.out.println(students);
 
-        array.removeElement(4);
-        System.out.println(array);
+        students.addLast(new Student("Dan", 90));
 
-        array.removeFirst();
-        System.out.println(array);
+        System.out.println(students);
     }
 }
