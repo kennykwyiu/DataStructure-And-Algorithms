13f92779131335bfbb46d873c6c58c8eb5a4475f
kennyyiu <kenny.kwyiu@gmail.com>
2025-06-16 21:40:01 +0800

    test ArrayStack

---
+++ src/main/java/org/kenny/data_structure/course/datastrutures/stack/Main.java
@@ -0,0 +1,15 @@
+package org.kenny.data_structure.course.datastrutures.stack;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayStack<Integer> stack = new ArrayStack<>();
+
+        for (int i = 0; i < 5; i++) {
+            stack.push(i);
+            System.out.println(stack);
+        }
+
+        stack.pop();
+        System.out.println(stack);
+    }
+}
