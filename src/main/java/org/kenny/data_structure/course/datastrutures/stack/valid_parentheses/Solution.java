47c058e7c45d947a516e7c8827d76065893f9f43
kennyyiu <kenny.kwyiu@gmail.com>
2025-06-17 22:06:18 +0800

    valid_parentheses

---
+++ src/main/java/org/kenny/data_structure/course/datastrutures/stack/valid_parentheses/Solution.java
@@ -0,0 +1,28 @@
+package org.kenny.data_structure.course.datastrutures.stack.valid_parentheses;
+
+import java.util.Stack;
+
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if (c == ')' && top != '(') {
+                    return false;
+                } else if (c == '}' && top != '{') {
+                    return false;
+                } else if (c == ']' && top != '[') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
