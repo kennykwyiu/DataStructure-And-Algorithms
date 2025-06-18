3db1804803fdd0109d0aae95a60f73d306432c37
kennyyiu <kenny.kwyiu@gmail.com>
2025-06-18 22:01:27 +0800

    add Queue

---
+++ src/main/java/org/kenny/data_structure/course/datastrutures/queue/Queue.java
@@ -0,0 +1,9 @@
+package org.kenny.data_structure.course.datastrutures.queue;
+
+public interface Queue<E> {
+    int getSize();
+    boolean isEmpty();
+    void enqueue(E e);
+    E dequeue();
+    E peek();
+}
