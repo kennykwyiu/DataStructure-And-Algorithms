54ddcb957f057dc0ab62b5c5edd44749ac4a4c19
kennyyiu <kenny.kwyiu@gmail.com>
2025-06-19 21:51:28 +0800

    LoopQueue

---
+++ src/main/java/org/kenny/data_structure/course/datastrutures/queue/LoopQueue.java
@@ -0,0 +1,20 @@
+package org.kenny.data_structure.course.datastrutures.queue;
+
+public class LoopQueue<E> implements Queue<E> {
+    private E[] data;
+    private int front, tail;
+    private int size;
+
+    public LoopQueue(int capacity) {
+        data = (E[]) new Object[capacity + 1];
+        front = 0;
+        tail = 0;
+        size = 0;
+    }
+
+    public LoopQueue() {
+        this(10);
+    }
+
+
+}
