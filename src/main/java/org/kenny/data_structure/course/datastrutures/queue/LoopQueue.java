9c4526b1aaa46c3ca28ee327c498dce0d391f950
kennyyiu <kenny.kwyiu@gmail.com>
2025-06-20 21:30:45 +0800

    add some tests

--- src/main/java/org/kenny/data_structure/course/datastrutures/queue/LoopQueue.java
+++ src/main/java/org/kenny/data_structure/course/datastrutures/queue/LoopQueue.java
@@ -90,4 +90,17 @@ public class LoopQueue<E> implements Queue<E> {
         sb.append("] tail");
         return sb.toString();
     }
+
+    public static void main(String[] args) {
+        LoopQueue<Integer> queue = new LoopQueue<>();
+        for (int i = 0; i < 10; i++) {
+            queue.enqueue(i);
+            System.out.println(queue);
+
+            if (i % 3 == 2) {
+                queue.dequeue();
+                System.out.println(queue);
+            }
+        }
+    }
 }
