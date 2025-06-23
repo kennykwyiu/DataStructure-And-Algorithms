2ad91b6817751d61fa9c6ed947c07a7b0497021e
kennyyiu <kenny.kwyiu@gmail.com>
2025-06-23 21:17:36 +0800

    MyQueue

---
+++ src/main/java/org/kenny/data_structure/course/datastrutures/queue/LC232ImplementQueueusingStacks/MyQueue.java
@@ -0,0 +1,11 @@
+package org.kenny.data_structure.course.datastrutures.queue.LC232ImplementQueueusingStacks;
+
+import java.util.Stack;
+
+public class MyQueue {
+    private Stack<Integer> stack;
+
+    public MyQueue() {
+        stack = new Stack<>();
+    }
+}
