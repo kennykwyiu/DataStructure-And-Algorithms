9d4c9cd53040030c6332f8d60cdb50ce5b5f8e68
kennyyiu <kenny.kwyiu@gmail.com>
2025-06-24 21:51:11 +0800

    MyQueue3

--- src/main/java/org/kenny/data_structure/course/datastrutures/queue/LC232ImplementQueueusingStacks/MyQueue3.java
+++ src/main/java/org/kenny/data_structure/course/datastrutures/queue/LC232ImplementQueueusingStacks/MyQueue3.java
@@ -11,4 +11,34 @@ public class MyQueue3 {
         stack1 = new Stack<>();
         stack2 = new Stack<>();
     }
+
+    public void push(int x) {
+        if (stack1.isEmpty()) {
+            front = x;
+        }
+        stack1.push(x);
+    }
+
+    public int pop() {
+        if (!stack2.isEmpty()) {
+            return stack2.pop();
+        }
+
+        while (stack1.size() > 1) {
+            stack2.push(stack1.pop());
+        }
+
+        return stack1.pop();
+    }
+
+    public int peek() {
+        if (!stack2.isEmpty()) {
+            return stack2.peek();
+        }
+        return front;
+    }
+
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
 }
