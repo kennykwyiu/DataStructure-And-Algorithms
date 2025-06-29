8f8ec4b77009b6b6d0c6c1a6e9be039fd5bd5ffc
kennyyiu <kenny.kwyiu@gmail.com>
2025-06-29 16:18:38 +0800

    dequeue

--- src/main/java/org/kenny/data_structure/course/datastrutures/linkedlist/ImplementQueueinLinkedList/LinkedListQueue.java
+++ src/main/java/org/kenny/data_structure/course/datastrutures/linkedlist/ImplementQueueinLinkedList/LinkedListQueue.java
@@ -57,4 +57,20 @@ public class LinkedListQueue <E> implements Queue<E> {
         }
         size++;
     }
+
+    @Override
+    public E dequeue() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("Cannot dequeue from an empty queue");
+        }
+
+        Node retNode = head;
+        head = head.next;
+        retNode.next = null;
+        if (head == null) {
+            tail = null;
+        }
+        size--;
+        return retNode.e;
+    }
 }
