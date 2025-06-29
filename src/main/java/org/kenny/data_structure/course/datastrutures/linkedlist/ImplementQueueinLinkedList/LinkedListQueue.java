45dab83c77daef81a7915ea40df9d150dfe5c834
kennyyiu <kenny.kwyiu@gmail.com>
2025-06-29 15:41:52 +0800

    LinkedListQueue

---
+++ src/main/java/org/kenny/data_structure/course/datastrutures/linkedlist/ImplementQueueinLinkedList/LinkedListQueue.java
@@ -0,0 +1,29 @@
+package org.kenny.data_structure.course.datastrutures.linkedlist.ImplementQueueinLinkedList;
+
+import org.kenny.data_structure.course.datastrutures.linkedlist.LinkedList;
+
+public class LinkedListQueue <E> implements Queue<E> {
+
+    private class Node {
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+}
