681fd0edc3a8077e2dc66b50d380de2db17e994e
kennyyiu <kenny.kwyiu@gmail.com>
2025-08-14 22:15:00 +0800

    add interface of Stack

---
+++ src/main/java/org/kenny/data_structure/course/datastrutures/linkedlist/ImplementStackinLinkedList/Stack.java
@@ -0,0 +1,9 @@
+package org.kenny.data_structure.course.datastrutures.linkedlist.ImplementStackinLinkedList;
+
+public interface Stack<E> {
+    int getSize();
+    boolean isEmpty();
+    void push(E e);
+    E pop();
+    E peek();
+}
