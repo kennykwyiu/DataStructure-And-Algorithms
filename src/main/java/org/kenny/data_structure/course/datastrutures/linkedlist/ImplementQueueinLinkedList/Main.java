3b0d23b838195419be446c07d9c870becd559898
kennyyiu <kenny.kwyiu@gmail.com>
2025-08-19 22:10:03 +0800

    test for LinkedListQueue

---
+++ src/main/java/org/kenny/data_structure/course/datastrutures/linkedlist/ImplementQueueinLinkedList/Main.java
@@ -0,0 +1,16 @@
+package org.kenny.data_structure.course.datastrutures.linkedlist.ImplementQueueinLinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedListQueue<Integer> queue = new LinkedListQueue<>();
+        for (int i = 0; i < 10; i++) {
+            queue.enqueue(i);
+            System.out.println(queue);
+
+            if (i % 3 == 2) {
+                queue.dequeue();
+                System.out.println(queue);
+            }
+        }
+    }
+}
