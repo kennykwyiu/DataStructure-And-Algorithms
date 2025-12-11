de18c8d1a07566b092225c3a3bc1571aac088430
kennyyiu <kenny.kwyiu@gmail.com>
2025-12-11 21:48:25 +0800

    added variables

--- src/main/java/org/kenny/data_structure/lowlatency/trading/orderbook/Order.java
+++ src/main/java/org/kenny/data_structure/lowlatency/trading/orderbook/Order.java
@@ -1,4 +1,9 @@
 package org.kenny.data_structure.lowlatency.trading.orderbook;
 
 public class Order {
+    private final long orderId;
+    private final String symbol;
+    private final double price;
+    private final int quantity;
+
 }
