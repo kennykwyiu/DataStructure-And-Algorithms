0cefb6798eab3cb7921e328902f4f619b8dbede3
kennyyiu <kenny.kwyiu@gmail.com>
2025-12-12 21:44:33 +0800

    added orders

--- src/main/java/org/kenny/data_structure/lowlatency/trading/orderbook/DirectArrayOrderBook.java
+++ src/main/java/org/kenny/data_structure/lowlatency/trading/orderbook/DirectArrayOrderBook.java
@@ -2,4 +2,6 @@ package org.kenny.data_structure.lowlatency.trading.orderbook;
 
 public class DirectArrayOrderBook {
     private static final int MAX_ORDERS = 1_000_000;
+    private Order[] orders = new Order[MAX_ORDERS];
+    }
 }
