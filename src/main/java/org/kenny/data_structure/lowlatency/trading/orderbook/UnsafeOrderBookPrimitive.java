c32e4e9b85a87b926a3281173bf15b7d3c88ebdb
kennyyiu <kenny.kwyiu@gmail.com>
2025-12-17 21:27:06 +0800

    added static block

--- src/main/java/org/kenny/data_structure/lowlatency/trading/orderbook/UnsafeOrderBookPrimitive.java
+++ src/main/java/org/kenny/data_structure/lowlatency/trading/orderbook/UnsafeOrderBookPrimitive.java
@@ -9,6 +9,17 @@ public class UnsafeOrderBookPrimitive {
     private static final long ORDER_STRUCT_SIZE = 32;
     private long baseAddress;
 
+    static {
+        try {
+            Field f = Unsafe.class.getDeclaredField("theUnsafe");
+            f.setAccessible(true);
+            unsafe = (Unsafe) f.get(null);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
     static class OrderData {
         long orderId;
         double price;
