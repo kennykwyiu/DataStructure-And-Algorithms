faccd74f7e2a26385788247d50797f7b405806fb
kennyyiu <kenny.kwyiu@gmail.com>
2025-12-22 22:30:53 +0800

    added warmup

--- src/main/java/org/kenny/data_structure/lowlatency/trading/orderbook/PerformanceBenchmark.java
+++ src/main/java/org/kenny/data_structure/lowlatency/trading/orderbook/PerformanceBenchmark.java
@@ -5,4 +5,15 @@ import java.util.Random;
 public class PerformanceBenchmark {
     private static final int WARMUP = 100_000;
     private static final int ITERATIONS = 1_000_000;
+
+    private static void warmup() {
+        System.out.println("Warming up JVM...");
+        HashMapOrderBook warmupMap = new HashMapOrderBook();
+        for (int i = 0; i < WARMUP; i++) {
+            warmupMap.addOrder(new Order(i, "TEST", 100.0, 100));
+            warmupMap.getOrder(i);
+        }
+        System.gc();
+        System.out.println("Warmup complete.\n");
+    }
 }
