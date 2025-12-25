9127d2def3f3221d44b276b9f110e1bb9cb1a9e7
kennyyiu <kenny.kwyiu@gmail.com>
2025-12-25 20:05:12 +0800

    added benchmarkUnsafePrimitiveRead

--- src/main/java/org/kenny/data_structure/lowlatency/trading/orderbook/PerformanceBenchmark.java
+++ src/main/java/org/kenny/data_structure/lowlatency/trading/orderbook/PerformanceBenchmark.java
@@ -113,4 +113,21 @@ public class PerformanceBenchmark {
         book.cleanup();
     }
 
+    private static void benchmarkUnsafePrimitiveRead() {
+        UnsafeOrderBookPrimitive book = new UnsafeOrderBookPrimitive(ITERATIONS);
+        for (int i = 0; i < ITERATIONS; i++) {
+            book.putOrder(i, 150.0 + i, 100);
+        }
+
+        Random rand = new Random(42);
+        long start = System.nanoTime();
+        for (int i = 0; i < ITERATIONS; i++) {
+            UnsafeOrderBookPrimitive.OrderData o = book.getOrder(rand.nextInt(ITERATIONS));
+        }
+        long elapsed = System.nanoTime() - start;
+        System.out.printf("Unsafe (Primitive) Read: %6d ms  (%5.2f ns/op)  ⚡ FASTEST\n",
+                elapsed / 1_000_000, (double) elapsed / ITERATIONS);
+        book.cleanup();
+    }
+
 }
