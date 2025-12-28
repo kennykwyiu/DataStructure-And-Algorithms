f9d8ee22ae997b25ce7e08730faf8de251a16f62
kennyyiu <kenny.kwyiu@gmail.com>
2025-12-28 23:29:47 +0800

    added Warmup to let JIT compiler optimize

--- src/main/java/org/kenny/data_structure/trading/platform/core/benchmark/PerformanceBenchmark.java
+++ src/main/java/org/kenny/data_structure/trading/platform/core/benchmark/PerformanceBenchmark.java
@@ -5,7 +5,10 @@ import java.util.Arrays;
 public class PerformanceBenchmark {
 
     public static BenchmarkResult benchmark(Runnable operation, int iterations, int warmupIterations) {
-
+        // Warmup to let JIT compiler optimize
+        for (int i = 0; i < warmupIterations; i++) {
+            operation.run();
+        }
 
     }
 }
