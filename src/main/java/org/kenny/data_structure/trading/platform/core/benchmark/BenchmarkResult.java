e8428b1ba10261a1283000a21b50d6808c551f72
kennyyiu <kenny.kwyiu@gmail.com>
2025-12-28 23:27:46 +0800

    added variables

--- src/main/java/org/kenny/data_structure/trading/platform/core/benchmark/BenchmarkResult.java
+++ src/main/java/org/kenny/data_structure/trading/platform/core/benchmark/BenchmarkResult.java
@@ -1,4 +1,14 @@
 package org.kenny.data_structure.trading.platform.core.benchmark;
 
 public class BenchmarkResult {
+    public int iterations;
+    public long totalTimeNanos;
+    public double throughput;
+    public long p50Latency;
+    public long p95Latency;
+    public long p99Latency;
+    public long maxLatency;
+    public long minLatency;
+    public long avgLatency;
+
 }
