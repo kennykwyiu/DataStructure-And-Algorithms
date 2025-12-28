a23fb599c23530f820c2766d29c4b67c4b9f029a
kennyyiu <kenny.kwyiu@gmail.com>
2025-12-28 23:29:28 +0800

    added benchmark

--- src/main/java/org/kenny/data_structure/trading/platform/core/benchmark/PerformanceBenchmark.java
+++ src/main/java/org/kenny/data_structure/trading/platform/core/benchmark/PerformanceBenchmark.java
@@ -3,4 +3,9 @@ package org.kenny.data_structure.trading.platform.core.benchmark;
 import java.util.Arrays;
 
 public class PerformanceBenchmark {
+
+    public static BenchmarkResult benchmark(Runnable operation, int iterations, int warmupIterations) {
+
+
+    }
 }
