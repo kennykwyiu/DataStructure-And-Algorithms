50fd381f79c1d6aa38f17a81a6c0af08b170043d
kennyyiu <kenny.kwyiu@gmail.com>
2026-01-02 22:47:35 +0800

    added variables

--- src/main/java/org/kenny/data_structure/trading/platform/core/lockfree/Order.java
+++ src/main/java/org/kenny/data_structure/trading/platform/core/lockfree/Order.java
@@ -1,4 +1,9 @@
 package org.kenny.data_structure.trading.platform.core.lockfree;
 
 public class Order {
+    long orderId;
+    String symbol;
+    int quantity;
+    double price;
+    String side; // "BUY" or "SELL"
 }
