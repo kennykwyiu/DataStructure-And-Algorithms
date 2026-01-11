9a0d1346a75bdcfea7d8dfe0658eb5566906c09e
kennyyiu <kenny.kwyiu@gmail.com>
2026-01-11 14:57:20 +0800

    add StreamCreation

---
+++ src/main/java/org/kenny/stream/StreamCreation.java
@@ -0,0 +1,13 @@
+package org.kenny.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * Demonstrates various ways to create streams in Java
+ */
+public class StreamCreation {
+}
