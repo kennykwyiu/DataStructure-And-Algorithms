234bf558408789aae9b0af15596cc39c7193133b
kennyyiu <kenny.kwyiu@gmail.com>
2026-01-13 22:45:58 +0800

    added Step 1: Create a list of names as the source data

--- src/main/java/org/kenny/stream/MapExample.java
+++ src/main/java/org/kenny/stream/MapExample.java
@@ -4,5 +4,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class MapExample {}
+public class MapExample {
+    public static void main(String[] args) {
+        // Step 1: Create a list of names as the source data
+        List<String> names = Arrays.asList("alice", "bob", "charlie");
 
+    }
+}
