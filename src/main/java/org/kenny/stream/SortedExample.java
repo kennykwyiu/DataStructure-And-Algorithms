3712fc17b6e920e382e4150b6fa17307ed3da4ae
kennyyiu <kenny.kwyiu@gmail.com>
2026-01-14 23:47:48 +0800

    added Step 1: Create a list of integers to demonstrate sorting

--- src/main/java/org/kenny/stream/SortedExample.java
+++ src/main/java/org/kenny/stream/SortedExample.java
@@ -1,5 +1,15 @@
 package org.kenny.stream;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class SortedExample {
+    public static void main(String[] args) {
+        // Step 1: Create a list of integers to demonstrate sorting
+        List<Integer> numbers = Arrays.asList(5, 3, 8, 1, 9, 2);
+
+
+    }
 }
