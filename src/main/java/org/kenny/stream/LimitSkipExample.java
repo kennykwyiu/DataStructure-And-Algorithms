c8fa792ad25719815df09dc96d85b798bd9137fc
kennyyiu <kenny.kwyiu@gmail.com>
2026-01-17 17:14:50 +0800

    added LimitSkipExample

---
+++ src/main/java/org/kenny/stream/LimitSkipExample.java
@@ -0,0 +1,9 @@
+package org.kenny.stream;
+
+
+/**
+ * This class demonstrates the use of limit() and skip() operations in Java Streams.
+ * These operations are useful for pagination, taking subsets of data, and controlling stream processing.
+ */
+public class LimitSkipExample {
+}
