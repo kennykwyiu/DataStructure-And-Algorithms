0032682c39dca03301b8b1db4e1804553aa29afd
kennyyiu <kenny.kwyiu@gmail.com>
2026-01-18 13:07:08 +0800

    added Step 1: Create a list of integers with duplicates

--- src/main/java/org/kenny/stream/DistinctExample.java
+++ src/main/java/org/kenny/stream/DistinctExample.java
@@ -1,5 +1,13 @@
 package org.kenny.stream;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class DistinctExample {
+    public static void main(String[] args) {
+        // Step 1: Create a list of integers with duplicates
+        List<Integer> numbers = Arrays.asList(1, 2, 2, 3, 3, 3, 4, 5, 5);
+
+    }
 }
