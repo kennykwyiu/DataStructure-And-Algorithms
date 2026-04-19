965ce1c54e8e274c50659b6431c840135891a40e
kennyyiu <kenny.kwyiu@gmail.com>
2026-04-20 00:03:06 +0800

    NameSplitter - inner class - Parts - variables and constructor

--- src/main/java/org/kenny/namesplitter/NameSplitter.java
+++ src/main/java/org/kenny/namesplitter/NameSplitter.java
@@ -2,6 +2,13 @@ package org.kenny.namesplitter;
 
 public class NameSplitter {
     public static class Parts {
+        public final String english;
+        public final String chinese;
+
+        public Parts(String english, String chinese) {
+            this.english = english;
+            this.chinese = chinese;
+        }
     }
 
 }
