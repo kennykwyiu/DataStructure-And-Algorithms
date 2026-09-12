b52fc7a9cf8c950265e2a03a61f57337d4452f82
kennyyiu <kenny.kwyiu@gmail.com>
2026-09-12 20:59:06 +0800

    createState and restoreState

--- src/main/java/org/kenny/designpattern/memento/Editor.java
+++ src/main/java/org/kenny/designpattern/memento/Editor.java
@@ -3,6 +3,14 @@ package org.kenny.designpattern.memento;
 public class Editor {
     private String content;
 
+    public EditorState createState() {
+        return new EditorState(content);
+    }
+
+    public void restoreState(EditorState state) {
+        content = state.getState();
+    }
+
     public String getContent() {
         return content;
     }
