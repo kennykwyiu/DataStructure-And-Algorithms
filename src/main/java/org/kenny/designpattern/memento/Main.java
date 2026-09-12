fd0c46b3772a738a545183a4a367e160ae89b74f
kennyyiu <kenny.kwyiu@gmail.com>
2026-09-12 21:10:38 +0800

    main

--- src/main/java/org/kenny/designpattern/memento/Main.java
+++ src/main/java/org/kenny/designpattern/memento/Main.java
@@ -3,8 +3,17 @@ package org.kenny.designpattern.memento;
 public class Main {
     public static void main(String[] args) {
         Editor editor = new Editor();
+        History history = new History();
+
         editor.setContent("a");
+        history.push(editor.createState());
+
         editor.setContent("b");
+        history.push(editor.createState());
+
         editor.setContent("c");
+        editor.restore(history.pop());
+
+        System.out.println(editor.getContent());
     }
 }
